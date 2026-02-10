a2f8d90ff49fa1afdbb497d13dc15169bcc08847
balddrAdmin <boliklevik@gmail.com>
2026-02-10 22:58:36 +0100

    User class

--- biblioteka3000/user.cs
+++ biblioteka3000/user.cs
@@ -1,4 +1,29 @@
 namespace biblioteka3000
 {
-    
+    public class User
+    {
+        private string _login;
+
+        public string Login
+        {
+            get => _login;
+            set => _login = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set => _password = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        
+        private List<Multimedia> _multimedias;
+
+        public List<Multimedia> Multimedias
+        {
+            get => _multimedias;
+            set => _multimedias = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
 }
